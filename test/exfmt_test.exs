eb6aa3295abd374bee9222f41c76d6d00d8db77c
Louis Pilfold <louis@lpil.uk>
2017-05-29 17:57:23 +0100

    Exfmt.check/2

--- test/exfmt_test.exs
+++ test/exfmt_test.exs
@@ -14,6 +14,7 @@ defmodule ExfmtTest do
     end
   end
 
+
   describe "format!/2" do
     test "formats valid source code" do
       assert Exfmt.format!("[1,2,3]") == "[1, 2, 3]\n"
@@ -22,6 +23,23 @@ defmodule ExfmtTest do
     test "throws on invalid syntax" do
       message = "syntax error before: ','"
       error = catch_error(Exfmt.format!(","))
+      assert %Exfmt.SyntaxError{line: 1, message: message} == error
+    end
+  end
+
+
+  describe "check/2" do
+    test "ok for correctly formatted code" do
+      assert Exfmt.check("[1, 2, 3]\n") == :ok
+    end
+
+    test "format_error for incorrectly formatted code" do
+      assert Exfmt.check("[1,\n2]\n") == {:format_error, "[1, 2]\n"}
+    end
+
+    test "returns error on invalid syntax" do
+      message = "syntax error before: ','"
+      error = Exfmt.check(",")
       assert %Exfmt.SyntaxError{line: 1, message: message} == error
     end
   end
